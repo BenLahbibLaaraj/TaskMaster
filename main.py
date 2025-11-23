b2d5d14186ec7490abac5566932ddc90e2cbf4e2
BenLahbibLaaraj <benlahbib.laaraj@student.vives.be>
2025-11-23 01:21:30 +0100

    custom logo

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import tasks.task
+import tasks.recurring_task
+
 print("TaskMaster\nMaster your day, one task at a time.")
 
+print("""
+##########################################################################
+#                                                                        #
+#  TTTTT   AAA   SSSSS  K   K  M   M   AAA   SSSSS  TTTTT  EEEEE  RRRRR  #
+#    T    A   A  S      K  K   MM MM  A   A  S        T    E      R   R  #
+#    T    AAAAA  SSSSS  KKK    M M M  AAAAA  SSSSS    T    EEEE   RRRR   #
+#    T    A   A      S  K  K   M   M  A   A      S    T    E      R  R   #
+#    T    A   A  SSSSS  K   K  M   M  A   A  SSSSS    T    EEEEE  R   R  #
+#                                                                        #
+#                  Master your day, one task at a time.                  #
+#                                                          	             #
+##########################################################################
+""")
--- tasks/recurring_task.py
+++ tasks/recurring_task.py
@@ -1,3 +1,5 @@
+from tasks.task import Task
+
 class RecurringTask(Task):
 	def __init__(self, title, frequency, description="", deadline=None):
 		super().__init__(title, description, deadline)
