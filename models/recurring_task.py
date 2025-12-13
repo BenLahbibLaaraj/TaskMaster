7d25df0e75effeda6b23cc815b4168428c0863e5
BenLahbibLaaraj <benlahbib.laaraj@student.vives.be>
2025-12-13 13:57:30 +0100

    use classes to check for validation

--- models/recurring_task.py
+++ models/recurring_task.py
@@ -1,6 +1,6 @@
-from tasks.task import Task
+from models.task import Task
 
 class RecurringTask(Task):
 	def __init__(self, title, frequency, description="", deadline=None):
 		super().__init__(title, description, deadline)
-		self.frequency = frequency
+		self.frequency = frequency if frequency != "" else None
--- services/add_recurring_task.py
+++ services/add_recurring_task.py
@@ -1,2 +1,18 @@
-def add_recurring_task():
-	print("add recurring task")
+from models.recurring_task import RecurringTask
+
+def add_recurring_task(connection):
+	title = ""
+	while title == "":
+		title = input("Give your recurring task a title: ")
+	description = input("\n\n(leave blank for no description)\nGive your recurring task a description: ")
+	deadline = input("\n\n(leave blank for no deadline and no frequency)\nGive your recurring task a deadline: ")
+	if deadline != "":
+		frequency = input("\n\n(leave blank for no frequency)\nGive your recurring task a frequency: ")
+	else:
+		frequency = ""
+
+	added_recurring_task = RecurringTask(title, description, deadline, frequency)
+
+	cursor = connection.cursor()
+	cursor.execute("INSERT INTO recurring_tasks(title, description, deadline, frequency) VALUES(?, ?, ?, ?)", (added_recurring_task.title, added_recurring_task.description, added_recurring_task.deadline, added_recurring_task.frequency))
+	connection.commit()
