8439fe023cd4d13cab25e8a5c435dfadc2beabae
BenLahbibLaaraj <benlahbib.laaraj@student.vives.be>
2025-12-22 00:48:53 +0100

    succesfully created correct csv files, now have to work on correctly assigning files to folders

--- services/manage_tasks/export_tasks.py
+++ services/manage_tasks/export_tasks.py
@@ -1,9 +1,55 @@
+import os
+import csv
+
 from services.manage_tasks.list_tasks import list_tasks
 
-def csv_xlsx():
-	return input("\nChoose file type:\n1 CSV\n2 Excel\n\n")
+def destination_folder():
+	return input("\n(leave empty for default folder 'TaskMaster/exports/', end folder structure with '/')\nChoose destination folder: ")
+
+def file_title():
+	return input("\nType filename (without file extension): ")
+
+def file_type():
+	return int(input("\nChoose file type:\n1 CSV\n2 Excel\n\n"))
+
+def check_if_recurring(tasks):
+	if len(tasks[0]) == 3:
+		return False
+	else:
+		return True
+
+def export_csv(filename, tasks):
+	with open(filename, mode="w", newline="", encoding="utf8") as file:
+		writer = csv.writer(file)
+		if check_if_recurring(tasks):
+			writer.writerow(["title", "description", "deadline", "frequency"])
+			writer.writerow([])
+			for task in tasks:
+				writer.writerow(task)
+		else:
+			writer.writerow(["title", "description", "deadline"])
+			writer.writerow([])
+			for task in tasks:
+				writer.writerow(task)
+
+#def export_xlsx(tasks):
 
 def export_tasks(connection):
-	file_type = csv_xlsx()
+	prefix = destination_folder()
+	if prefix == "":
+		prefix = "exports/"
+	interfix = file_title()
+	suffix = file_type()
+	if prefix == "exports/":
+		if suffix == 1:
+			prefix = prefix + "csv/"
+		else:
+			prefix = prefix + "xlsx/"
 	task_type = list_tasks(connection)
 	print(task_type)
+	if suffix == 1:
+		filename = prefix + interfix + ".csv"
+		export_csv(filename, task_type)
+	else:
+		file = interfix + ".xlsx"
+		#export_xlsx(filename, task_type)
