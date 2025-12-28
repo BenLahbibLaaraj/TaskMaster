4842a7e744a889a95053c798bfb671bcca31fe3f
BenLahbibLaaraj <benlahbib.laaraj@student.vives.be>
2025-12-28 02:45:10 +0100

    print every task with spaced out cols

--- services/manage_tasks/print_tasks.py
+++ services/manage_tasks/print_tasks.py
@@ -1,11 +1,16 @@
 def print_tasks(list_setting, tasks):
 	print("\n\n(events are shown earliest first)")
-	
+
 	if list_setting == 1:
 		print("Tasks:\n")
+
+		for task in tasks:
+			print(f"{task[0]}\t{task[1]}\t{task[2]}")
+		print("\n")
+		
 	if list_setting == 2:
 		print("Recurring tasks:\n")
 
-	for task in tasks:
-		print(task)
-	print("\n")
+		for task in tasks:
+			print(f"{task[0]}\t{task[1]}\t{task[2]}\t{task[3]}")
+		print("\n")
