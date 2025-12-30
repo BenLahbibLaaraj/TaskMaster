d2ff32225f5d9c810e658785bb52a6628692bb37
BenLahbibLaaraj <benlahbib.laaraj@student.vives.be>
2025-12-30 23:15:18 +0100

    automatically redirect recurring task with no deadline to normal task

--- services/add_tasks/add_recurring_task.py
+++ services/add_tasks/add_recurring_task.py
@@ -1,3 +1,4 @@
+from models.task import Task
 from models.recurring_task import RecurringTask
 from services.add_tasks.validate_deadline import is_deadline_valid
 
@@ -12,11 +13,15 @@ def add_recurring_task(connection):
 			break
 	if deadline != "":
 		frequency = input("\n\n(leave blank for no frequency)\nGive your recurring task a frequency: ")
+
+		added_recurring_task = RecurringTask(title, description, deadline, frequency)
+
+		cursor = connection.cursor()
+		cursor.execute("INSERT INTO recurring_tasks(title, description, deadline, frequency) VALUES(?, ?, ?, ?)", (added_recurring_task.title, added_recurring_task.description, added_recurring_task.deadline, added_recurring_task.frequency))
+		connection.commit()
 	else:
-		frequency = ""
+		added_task = Task(title, description, deadline)
 
-	added_recurring_task = RecurringTask(title, description, deadline, frequency)
-
-	cursor = connection.cursor()
-	cursor.execute("INSERT INTO recurring_tasks(title, description, deadline, frequency) VALUES(?, ?, ?, ?)", (added_recurring_task.title, added_recurring_task.description, added_recurring_task.deadline, added_recurring_task.frequency))
-	connection.commit()
+		cursor = connection.cursor()
+		cursor.execute("INSERT INTO tasks(title, description, deadline) VALUES(?, ?, ?)", (added_task.title, added_task.description, added_task.deadline))
+		connection.commit()
