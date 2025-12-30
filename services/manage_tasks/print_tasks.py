23c390565e97988ea9325379ab9e96b67abaf5bf
BenLahbibLaaraj <benlahbib.laaraj@student.vives.be>
2025-12-30 22:19:53 +0100

    simplified printing tasks

--- services/manage_tasks/print_tasks.py
+++ services/manage_tasks/print_tasks.py
@@ -1,24 +1,24 @@
+import datetime
+
 def print_tasks(connection, list_setting, statement, tasks):
 	cursor = connection.cursor()
 
-	print("\n\n(events are shown earliest first)")
+	GREEN = '\033[32m'
+	YELLOW = '\033[33m'
+	RED = '\033[31m'
+	RESET = '\033[0m'
+
+	print("\n\n(tasks are shown earliest first)")
 
 	if list_setting == 1:
 		print("Tasks:\n")
 
-		cursor.execute(statement)
-		column_names = [description[0] for description in cursor.description]
-		print("\t".join(column_names))
-
-		for task in tasks:
-			print("\t".join(str(value) for value in task))
-
 	if list_setting == 2:
 		print("Recurring tasks:\n")
 
-		cursor.execute(statement)
-		column_names = [description[0] for description in cursor.description]
-		print("\t".join(column_names))
+	cursor.execute(statement)
+	column_names = [description[0] for description in cursor.description]
+	print("\t".join(column_names))
 
-		for task in tasks:
-			print("\t".join(str(value) for value in task))
+	for task in tasks:
+		print("\t".join(str(value) for value in task))
